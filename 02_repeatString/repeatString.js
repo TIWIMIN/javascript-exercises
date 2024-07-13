fde32c450571a717c5c8b20c9fbf46aa52e8564d
TIWIMIN <tiwimin@gmail.com>
2024-07-12 17:26:27 -0700

    Pass 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let solution = "";
+    if (num < 0) {
+        return "ERROR";
+    }
+    for (let i = 0; i < num; i++) {
+        solution += string;
+    }
+    return solution;
 };
 
 // Do not edit below this line
