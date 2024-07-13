be675abcaa7de064e2d88f135ed12ccd6a1f1c6c
TIWIMIN <tiwimin@gmail.com>
2024-07-12 17:42:26 -0700

    Pass 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let solution = ""; 
+    for (let i = string.length - 1; i >= 0; i--) {
+        solution += string.at(i); 
+    }
 
+    return solution;
 };
 
 // Do not edit below this line
