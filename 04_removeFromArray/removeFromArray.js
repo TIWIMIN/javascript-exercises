5cce9979113e3f8d969ce9680c75379bd07b2f05
TIWIMIN <tiwimin@gmail.com>
2024-07-12 17:57:15 -0700

    Pass 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    removeList = new Set();
+    solution = [];
+    for (let i = 1; i < arguments.length; i++) {
+        removeList.add(arguments[i]); 
+    }
+
+    for (const element of array) {
+        if (!removeList.has(element)) {
+            solution.push(element); 
+        }
+
+    }
+
+    return solution;
 };
 
 // Do not edit below this line
