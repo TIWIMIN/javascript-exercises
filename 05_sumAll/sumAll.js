38f082e9eeee59854ad0d17a630ea9f8d5cbd4dd
TIWIMIN <tiwimin@gmail.com>
2024-07-12 18:06:48 -0700

    Pass 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+    let smallNum = firstNum; 
+    let bigNum = secondNum; 
+    let solution = 0;
 
+    if (firstNum < 0 || secondNum < 0) {
+        return "ERROR";
+    }
+
+    if (typeof firstNum !== "number" || typeof secondNum !== "number") {
+        return "ERROR";
+    }
+
+    // If the arguments are passed in non-ascending order, it must be accounted for
+    if (firstNum > secondNum) {
+        smallNum = secondNum; 
+        bigNum  = firstNum; 
+    }
+
+    for (let i = smallNum; i <= bigNum; i++) {
+        solution += i; 
+    }
+
+    return solution;
 };
 
 // Do not edit below this line
