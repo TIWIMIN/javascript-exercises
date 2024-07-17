ac3eb454a9f193645579ff3da5b7aa16d0dfb115
TIWIMIN <tiwimin@gmail.com>
2024-07-17 16:20:36 -0700

    Solve 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+	const sol = a.reduce((total, element) => {
+    return total + element;
+  }, 0)
+  return sol;
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  const sol = a.reduce((product, element) => {
+    return product * element; 
+  }, 1)
+  return sol; 
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  sol = 1; 
+	for (let i = 1; i <= a; i++) {
+    sol *= i;
+  }
+  return sol;
 };
 
 // Do not edit below this line
