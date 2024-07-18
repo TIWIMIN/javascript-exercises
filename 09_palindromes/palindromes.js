5211184d4b9c0cea8c906c1cb7749987cdffeb12
TIWIMIN <tiwimin@gmail.com>
2024-07-17 17:54:40 -0700

    Solve 09_palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (drome) {
+    let dromeShrink = [...drome].map((char) => {
+        return char.toLowerCase();
+    })
 
+    dromeShrink = dromeShrink.filter((char) => {
+        return !(char === ' ') ; 
+    })
+
+    dromeReversed = dromeShrink.toReversed(); 
+    dromeShrink = dromeShrink.toString().replace(/[^A-Za-z0-9]/g, '');
+    dromeReversed = dromeReversed.toString().replace(/[^A-Za-z0-9]/g, '');
+
+    console.log(dromeShrink + "normal");
+    console.log(dromeReversed + "reversed");
+    return (dromeShrink === dromeReversed);
 };
 
 // Do not edit below this line
