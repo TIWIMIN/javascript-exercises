71a5eed50a8f8497c4c2b5612d03205c0b67e255
TIWIMIN <tiwimin@gmail.com>
2024-07-17 18:28:52 -0700

    Solve 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const oldest = people.sort((personA, personB) => {
+        if (personA.yearOfDeath === undefined) {
+            personA.yearOfDeath = (new Date()).getFullYear();
+        }
+        if (personB.yearOfDeath === undefined) {
+            personB.yearOfDeath = (new Date()).getFullYear();
+        }
 
+        ageA = personA.yearOfDeath - personA.yearOfBirth;
+        ageB = personB.yearOfDeath - personB.yearOfBirth;
+        return ((ageA > ageB) ? -1 : 1);
+    })[0]
+
+    return oldest;
 };
 
 // Do not edit below this line
