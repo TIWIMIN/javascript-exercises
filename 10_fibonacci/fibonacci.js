9b6b05bc8e120a12e580c329463952f403f819d6
TIWIMIN <tiwimin@gmail.com>
2024-07-17 18:11:39 -0700

    Solve 10_finonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    if (index === 0 || index === "0") {
+        return 0;
+    }
 
+    if (index < 0) {
+        return "OOPS";
+    }
+
+    let [first, second] = [0, 1]; 
+    for (let i = 0; i < (index - 1); i++) {
+        let temp = first; 
+        first = second; 
+        second = temp + first; 
+    }
+    return second;
 };
 
 // Do not edit below this line
